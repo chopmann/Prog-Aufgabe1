42c0723d24b54b1cb1f7c50d7b11a1b77623fae4
Cornelio Hopmann <hopmann@gmail.com>
2013-09-30 21:47:46 +0200

    Feature: Add a Plant to a Random Location
    Added a Helper Method for generating a Random Integer in a range, all
    credits go to http://stackoverflow.com/users/462/greg-case.
    Added some Test for the new Feature.

--- Prog-Aufgabe1/src/de/proglabor/aufgabe1/World.java
+++ Prog-Aufgabe1/src/de/proglabor/aufgabe1/World.java
@@ -3,6 +3,8 @@
  */
 package de.proglabor.aufgabe1;
 
+import java.util.Random;
+
 /**
  * @author sirmonkey
  * 
@@ -44,12 +46,65 @@ public class World {
 	public void addPlant(int x, int y) {
 		plantContainer[x][y]++;
 	}
+	
+	/**
+	 * Add a Plant at a Random Location inside the World
+	 */
+	public void randomAddPlant() {
+		Random rand = new Random();
+		int x = randInt(0, width-1, rand);
+		int y = randInt(0, height-1, rand);
+		addPlant(x, y);
+
+	}
+	
+	
+	/**
+	 * @param x Coordinate
+	 * @param y Coordinate
+	 * @return Plant count at a Given Coordinate
+	 */
+	public int countPlants(int x, int y) {
+		return plantContainer[x][y];
+	}
+	
+	/**
+	 * @return The Total Plant Count
+	 */
+	public int totalPlants() {
+		int total = 0;
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < height; j++) {
+				total += countPlants(i, j);
+			}
+		}
+		return total;
+	}
 
 	/**
 	 * @return the plantContainer
 	 */
 	public int[][] getPlantContainer() {
 		return plantContainer;
+	}
+	
+	/**
+	 * http://stackoverflow.com/questions/363681/generating-random-numbers-in-a-range-with-java
+	 * Returns a psuedo-random number between min and max, inclusive.
+	 * The difference between min and max can be at most
+	 * <code>Integer.MAX_VALUE - 1</code>.
+	 *
+	 * @param min Minimim value
+	 * @param max Maximim value.  Must be greater than min.
+	 * @return Integer between min and max, inclusive.
+	 * @see java.util.Random#nextInt(int)
+	 */
+	public static int randInt(int min, int max, Random rand) {
+
+	    // nextInt is normally exclusive of the top value,
+	    // so add 1 to make it inclusive
+	    int randomNum = rand.nextInt((max - min) + 1) + min;
+	    return randomNum;
 	}
 
 }
--- Prog-Aufgabe1/src/de/proglabor/aufgabe1/test/WorldTest.java
+++ Prog-Aufgabe1/src/de/proglabor/aufgabe1/test/WorldTest.java
@@ -13,8 +13,26 @@ public class WorldTest {
 		int height = 40;
 		World dieWelt = new World(width, height);
 		dieWelt.addPlant(15, 20);
+		dieWelt.addPlant(20, 15);
+		dieWelt.addPlant(20, 15);
 		int[][] container = dieWelt.getPlantContainer();
+		assertEquals(0, container[0][0]);
 		assertEquals(1, container[15][20]);
+		assertEquals(2, container[20][15]);
+	}
+	
+	@Test
+	public void testTotalPlants() {
+		int width = 30;
+		int height = 40;
+		World dieWelt = new World(width, height);
+		//Add 5 Plants
+		dieWelt.addPlant(15, 20);
+		dieWelt.addPlant(15, 20);
+		dieWelt.addPlant(15, 20);
+		dieWelt.addPlant(20, 15);
+		dieWelt.addPlant(20, 15);
+		assertEquals(5, dieWelt.totalPlants());
 	}
 
 }
