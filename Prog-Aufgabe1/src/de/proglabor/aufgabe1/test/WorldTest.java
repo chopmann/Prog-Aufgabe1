cb1348f95e89c605ffc4dbc716f9651a3e243b98
Cornelio Hopmann <hopmann@gmail.com>
2013-10-01 23:21:28 +0200

    Fix: Test were against fixed Values and not against Parameters
    WORLD.setJungleLimits() WORKING! The Test were not updated to accept
    parameterized Values...a test with a bug...oh the irony :-D

--- Prog-Aufgabe1/src/de/proglabor/aufgabe1/test/WorldTest.java
+++ Prog-Aufgabe1/src/de/proglabor/aufgabe1/test/WorldTest.java
@@ -18,28 +18,34 @@ public class WorldTest {
 	World dieWelt;
 	int widthJungle;
 	int heightJungle;
+	int expectedX1;
+	int expectedY1;
 
-	public WorldTest(int width, int height, int widthJungle, int heightJungle) {
+	public WorldTest(int width, int height, int widthJungle, int heightJungle,
+			int expectedX1, int expectedY1) {
+
 		this.widthJungle = widthJungle;
 		this.heightJungle = heightJungle;
+		this.expectedX1 = expectedX1;
+		this.expectedY1 = expectedY1;
 		dieWelt = new World(width, height, widthJungle, heightJungle);
 	}
 
 	@Parameters
 	public static Collection<Object[]> createTestParams() {
-		return Arrays.asList(new Object[][] { { 40, 30, 10, 10 },
-				{ 39, 30, 10, 10 }, { 40, 29, 10, 10 }, { 30, 40, 11, 10 },
-				{ 30, 40, 10, 11 } });
+		return Arrays.asList(new Object[][] { { 40, 30, 10, 10, 15, 10 },
+				{ 40, 27, 10, 10, 15, 8 }, { 40, 30, 11, 10, 14, 10 },
+				{ 40, 30, 10, 11, 15, 9 }, { 40, 30, 11, 11, 14, 9 } });
 	}
 
 	@Test
 	public void testSetJungleLimits() {
 		dieWelt.setJungleLimits();
-		assertEquals("getJungleLimitY1", 10, dieWelt.getJungleLimitY1());
-		assertEquals("getJungleLimitY2", 10 + heightJungle,
+		assertEquals("getJungleLimitY1", expectedY1, dieWelt.getJungleLimitY1());
+		assertEquals("getJungleLimitY2", expectedY1 + heightJungle,
 				dieWelt.getJungleLimitY2());
-		assertEquals("getJungleLimitX1", 15, dieWelt.getJungleLimitX1());
-		assertEquals("getJungleLimitX2", 15 + widthJungle,
+		assertEquals("getJungleLimitX1", expectedX1, dieWelt.getJungleLimitX1());
+		assertEquals("getJungleLimitX2", expectedX1 + widthJungle,
 				dieWelt.getJungleLimitX2());
 	}
 
@@ -56,11 +62,6 @@ public class WorldTest {
 
 	@Test
 	public void testTotalPlants() {
-		int width = 40;
-		int height = 30;
-		int widthJungle = 10;
-		int heightJungle = 10;
-		World dieWelt = new World(width, height, widthJungle, heightJungle);
 		// Add 5 Plants
 		dieWelt.addPlant(15, 20);
 		dieWelt.addPlant(15, 20);
