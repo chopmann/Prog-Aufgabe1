90f98f4a2ecc3f90be994264bc8bc9b8fa1b7b38
Cornelio Hopmann <hopmann@gmail.com>
2013-10-01 20:07:31 +0200

    Added Parameterized Tests
    Following TestCases have been added:
     *Odd World Width
     *Odd World Height
     *Odd Jungle Width
     *Odd Jungle Height
    FAIL: The Method WORLD.setJungleLimit() is NOT working as intended!

--- Prog-Aufgabe1/src/de/proglabor/aufgabe1/test/WorldTest.java
+++ Prog-Aufgabe1/src/de/proglabor/aufgabe1/test/WorldTest.java
@@ -1,33 +1,50 @@
 package de.proglabor.aufgabe1.test;
 
 import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.Collection;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 import de.proglabor.aufgabe1.World;
 
+@RunWith(Parameterized.class)
 public class WorldTest {
-	
+
+	World dieWelt;
+	int widthJungle;
+	int heightJungle;
+
+	public WorldTest(int width, int height, int widthJungle, int heightJungle) {
+		this.widthJungle = widthJungle;
+		this.heightJungle = heightJungle;
+		dieWelt = new World(width, height, widthJungle, heightJungle);
+	}
+
+	@Parameters
+	public static Collection<Object[]> createTestParams() {
+		return Arrays.asList(new Object[][] { { 40, 30, 10, 10 },
+				{ 39, 30, 10, 10 }, { 40, 29, 10, 10 }, { 30, 40, 11, 10 },
+				{ 30, 40, 10, 11 } });
+	}
+
 	@Test
-	public void testSetJungleLimits(){
-		int width = 40;
-		int height = 30;
-		int widthJungle = 10;
-		int heightJungle = 10;
-		World dieWelt = new World(width, height, widthJungle,heightJungle);
+	public void testSetJungleLimits() {
 		dieWelt.setJungleLimits();
-		assertEquals(10, dieWelt.getJungleLimitY1());
-		assertEquals(10 + heightJungle, dieWelt.getJungleLimitY2());
-		assertEquals(15, dieWelt.getJungleLimitX1());
-		assertEquals(15 + widthJungle, dieWelt.getJungleLimitX2());
+		assertEquals("getJungleLimitY1", 10, dieWelt.getJungleLimitY1());
+		assertEquals("getJungleLimitY2", 10 + heightJungle,
+				dieWelt.getJungleLimitY2());
+		assertEquals("getJungleLimitX1", 15, dieWelt.getJungleLimitX1());
+		assertEquals("getJungleLimitX2", 15 + widthJungle,
+				dieWelt.getJungleLimitX2());
 	}
 
 	@Test
 	public void testAddPlant() {
-		int width = 40;
-		int height = 30;
-		int widthJungle = 10;
-		int heightJungle = 10;
-		World dieWelt = new World(width, height, widthJungle,heightJungle);
 		dieWelt.addPlant(15, 20);
 		dieWelt.addPlant(20, 15);
 		dieWelt.addPlant(20, 15);
@@ -36,15 +53,15 @@ public class WorldTest {
 		assertEquals(1, container[15][20]);
 		assertEquals(2, container[20][15]);
 	}
-	
+
 	@Test
 	public void testTotalPlants() {
 		int width = 40;
 		int height = 30;
 		int widthJungle = 10;
 		int heightJungle = 10;
-		World dieWelt = new World(width, height, widthJungle,heightJungle);
-		//Add 5 Plants
+		World dieWelt = new World(width, height, widthJungle, heightJungle);
+		// Add 5 Plants
 		dieWelt.addPlant(15, 20);
 		dieWelt.addPlant(15, 20);
 		dieWelt.addPlant(15, 20);
