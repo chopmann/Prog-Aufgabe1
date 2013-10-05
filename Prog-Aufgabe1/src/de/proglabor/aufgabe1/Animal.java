2cb44a4db0699f88720471d87fbac69b45d93f4a
Cornelio Hopmann <hopmann@gmail.com>
2013-10-05 11:17:59 +0200

    Added Several Animal Properties.
    The Animals have now Genes, an Energy Counter, a "Looking at" Direction
    and the simple ability to have children. The initial Genes Method is a
    first attempt.

--- Prog-Aufgabe1/src/de/proglabor/aufgabe1/Animal.java
+++ Prog-Aufgabe1/src/de/proglabor/aufgabe1/Animal.java
@@ -3,6 +3,8 @@
  */
 package de.proglabor.aufgabe1;
 
+import java.util.Random;
+
 /**
  * @author sirmonkey
  *
@@ -10,13 +12,46 @@ package de.proglabor.aufgabe1;
 public class Animal {
 	int posX;
 	int posY;
+	int energy;
+	int dir;
+	int[] genes;
 	
-	public Animal(int posX, int posY) {
+	public Animal(int energy, int posX, int posY) {
 		this.posX = posX;
 		this.posY = posY;
+		this.energy = energy;
+		this.dir = 0;
 	}
-
-
+	
+	public void initGenes() {
+		genes = new int[8];
+		Random rand = new Random();
+		for (int i = 0; i < genes.length; i++) {
+			genes[i] = Helper.randInt(1, 10, rand);
+		}
+	}
+	
+	public void setGenes(int[] genes) {
+		this.genes = genes;
+	}
+	
+	public int[] getGenes() {
+		return genes;
+	}
+	
+	public void setDir(int dir ) {
+		this.dir = dir;
+	}
+	
+	public int getDir(){
+		return dir;
+	}
+	
+	public Animal reproduce() {
+		Animal newBorn = new Animal(this.energy/2, this.posX, this.posY);
+		return newBorn;
+	}
+	
 	
 	
 }
