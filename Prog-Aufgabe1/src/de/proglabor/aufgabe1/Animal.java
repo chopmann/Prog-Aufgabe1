a8ef5a752e1c8bb9c618921ee9a910a48b565d58
Cornelio Hopmann <hopmann@gmail.com>
2013-10-06 21:16:55 +0200

    Feat: Animals can have children
    When an Animal reproduce itself you can choose between a random Mutated
    Gene or Play Scientist an add a Mutation yourself.

--- Prog-Aufgabe1/src/de/proglabor/aufgabe1/Animal.java
+++ Prog-Aufgabe1/src/de/proglabor/aufgabe1/Animal.java
@@ -1,13 +1,10 @@
-/**
- * 
- */
 package de.proglabor.aufgabe1;
 
 import java.util.Random;
 
 /**
  * @author sirmonkey
- *
+ * 
  */
 public class Animal {
 	int posX;
@@ -15,14 +12,14 @@ public class Animal {
 	int energy;
 	int dir;
 	int[] genes;
-	
+
 	public Animal(int energy, int posX, int posY) {
 		this.posX = posX;
 		this.posY = posY;
 		this.energy = energy;
 		this.dir = 0;
 	}
-	
+
 	public void initGenes() {
 		genes = new int[8];
 		Random rand = new Random();
@@ -30,31 +27,61 @@ public class Animal {
 			genes[i] = Helper.randInt(1, 10, rand);
 		}
 	}
-	
+
 	public void setGenes(int[] genes) {
 		this.genes = genes;
 	}
-	
+
 	public int[] getGenes() {
 		return genes;
 	}
-	
+
 	public void eat(int plantEnergy) {
 		energy += plantEnergy;
 	}
-	public void setDir(int dir ) {
+
+	public void setDir(int dir) {
 		this.dir = dir;
 	}
-	
-	public int getDir(){
+
+	public int getDir() {
 		return dir;
 	}
-	
+
+	/**
+	 * @return a new Born Animal with Random mutated Genes
+	 */
 	public Animal reproduce() {
-		Animal newBorn = new Animal(this.energy/2, this.posX, this.posY);
+		Random rand = new Random();
+		int randomGene = Helper.randInt(0, 7, rand);
+		int randomMutation = Helper.randInt(-1, 1, rand);
+		return reproduce(randomGene, randomMutation);
+	}
+
+	/**
+	 * @param randomGene
+	 * @param randomMutation
+	 * @return a new Born Animal with mutated Genes
+	 */
+	public Animal reproduce(int randomGene, int randomMutation) {
+		Animal newBorn = new Animal(this.energy / 2, this.posX, this.posY);
+		int[] newBornGenes = this.genes.clone();
+		newBorn.setGenes(newBornGenes);
+		newBorn.mutate(randomGene, randomMutation);
 		return newBorn;
 	}
-	
-	
-	
+
+	/**
+	 * @param gene
+	 * @param mutation
+	 */
+	public void mutate(int gene, int mutation) {
+		int mutatedGene = genes[gene] + mutation;
+		if (mutatedGene <= 0) {
+			genes[gene] = 0;
+		} else {
+			genes[gene] = mutatedGene;
+		}
+	}
+
 }
