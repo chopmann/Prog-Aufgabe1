12aef1c3d967f0ccf68c10ccccbddd9026e783be
Cornelio Hopmann <hopmann@gmail.com>
2013-10-12 14:49:28 +0200

    Added Course Public Test - Stub

---
+++ Prog-Aufgabe1/src/de/proglabor/aufgabe1/SimArray.java
@@ -0,0 +1,24 @@
+package de.proglabor.aufgabe1;
+
+public class SimArray implements SimArrayInterface {
+
+	@Override
+	public void day(int randomDirection, int randomReproduceGene,
+			int randomReproduceMutation) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int getAnzahlTiere(int x, int y) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getAnzahlPflanzen(int x, int y) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
